51080f5dc4be831d40828044b774791139ae7166
ramonespier <ramon.coelhomelo@outlook.com>
2025-08-25 10:36:45 -0300

    explicações basicas de tailwind e atividade leve

---
+++ atividade1/src/app/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+export default function Header() {
+    return (
+            <header className="container mx-auto">
+                <nav className="h-15 bg-white">
+                    <h1 className="text-black font-bold">grammarly</h1>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </nav>
+            </header>
+    )
+}
