5788a1f72ec982e3e36dec72b595a4d4486d527a
ramonespier <ramon.coelhomelo@outlook.com>
2025-08-27 21:41:24 -0300

    just styling

--- atividade1/src/app/components/Header/Header.jsx
+++ atividade1/src/app/components/Header/Header.jsx
@@ -1,42 +1,80 @@
-import { RiArrowDropDownLine } from "react-icons/ri";
-import { SiGrammarly } from 'react-icons/si';
+import { RiArrowDropDownLine, RiMenu3Line } from "react-icons/ri";
+import { SiGrammarly } from "react-icons/si";
 
 export default function Header() {
     return (
-        <header className="mx-auto flex flex-col">
-            <nav className="h-16 bg-white flex px-15 gap-10 items-center border-b border-b-neutral-300/90">
-                <div className=" container mx-auto flex items-center gap-15">
+        <header className="w-full flex flex-col">
+            <nav className="bg-white flex flex-col md:flex-row items-center justify-between border-b border-neutral-300/90 gap-4 md:gap-8 px-4 md:px-16 py-2 md:h-16 font-sans">
 
+                <div className="w-full md:w-auto flex items-center justify-between md:justify-start gap-4 md:gap-10 flex-wrap">
                     <div className="flex gap-2 items-center">
-                        <SiGrammarly size={40} color="#15C39A" />
-                        <h1 className="text-black font-extrabold text-xl">grammarly</h1>
+                        <SiGrammarly className="text-[#15C39A]" size="2.5rem" />
+                        <h1 className="text-black font-extrabold text-2xl">grammarly</h1>
                     </div>
 
-                    <ul className="flex gap-14 text-black">
-                        <li className="flex items-center">Why Grammarly<RiArrowDropDownLine size={30} /></li>
-                        <li className="flex items-center">For Work<RiArrowDropDownLine size={30} /></li>
-                        <li className="flex items-center">For Education<RiArrowDropDownLine size={30} /></li>
-                        <li className="flex items-center">Compare Plans</li>
-                        <li className="flex items-center">Tools & Guides<RiArrowDropDownLine size={30} /></li>
+                    <ul className="hidden 2xl:flex gap-8 text-black text-sm md:text-lg items-center">
+                        <li className="flex items-center hover:text-gray-700">
+                            <a href="#" className="flex">
+                                Why Grammarly
+                                <RiArrowDropDownLine size="1.8rem" />
+                            </a>
+                        </li>
+                        <li className="flex items-center hover:text-gray-700">
+                            <a href="#" className="flex">
+                                For Work
+                                <RiArrowDropDownLine size="1.8rem" />
+                            </a>
+                        </li>
+                        <li className="flex items-center hover:text-gray-700">
+                            <a href="#" className="flex">
+                                For Education
+                                <RiArrowDropDownLine size="1.8rem" />
+                            </a>
+                        </li>
+                        <li className="flex items-center hover:text-gray-700">
+                            <a href="#">Compare Plans</a>
+                        </li>
+                        <li className="flex items-center hover:text-gray-700">
+                            <a href="#" className="flex">
+                                Tools & Guides
+                                <RiArrowDropDownLine size="1.8rem" />
+                            </a>
+                        </li>
                     </ul>
+
+                    <button className="block 2xl:hidden text-black focus:outline-none">
+                        <RiMenu3Line size="1.75rem" />
+                    </button>
                 </div>
 
-                <div className="flex justify-between gap-10 text-black mx-auto font-bold">
-                    <button className="">
-                        <a className="w-full" href="#">My Grammarly</a>
+                <div className="w-full md:w-auto flex justify-around md:justify-end gap-4 font-bold flex-wrap mt-2 md:mt-0">
+                    <button className="py-2 px-4 hover:bg-gray-100 rounded-md text-black font-bold">
+                        <a href="#">My Grammarly</a>
                     </button>
-                    <button className="">
+                    <button className="bg-green-700 text-white py-2 px-4 rounded-md hover:bg-green-600 transition-colors duration-200 shadow-md">
                         <a className="w-full" href="#">Start a Free Trial</a>
                     </button>
                 </div>
-
             </nav>
-            <section>
-                <div className="h-14 bg-neutral-100">
 
+            <section>
+                <div className="h-14 bg-neutral-100 text-black font-sans flex items-center">
+
+                    <div>
+                        <h3 className="font-bold text-2xl">Business</h3>
+                    </div>
+
+                    <div>
+                        <ul>
+                            <li><a href="#">Pricing</a></li>
+                            <li><a href="#"></a></li>
+                            <li></li>
+                            <li></li>
+                        </ul>
+                    </div>
 
                 </div>
             </section>
         </header>
-    )
+    );
 }
